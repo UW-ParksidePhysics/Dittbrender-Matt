1e803228f87992f880cf9486bc90a1720c9f0f7b
Matt <dittb001@rangers.uwp.edu>
2020-04-24 13:28:45 -0500

    Completing assignments

---
+++ 5.31_wavepacket_movie_01.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
+from numpy import exp, pi, sqrt, linspace
+
+fig = plt.figure
+
+def f(x, m, s):
+    return (1.0)
